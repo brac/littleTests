bca3e97215514e9ce0378c686454849a51c317ba
Ben Bracamonte <ben.bracamonte@gmail.com>
2019-01-13 17:52:43 -0600

    configure testing environment, multiple test suites

--- public/javascripts/bitEngine.js
+++ public/javascripts/bitEngine.js
@@ -1,50 +1,14 @@
 const concatStrings = (str1, str2) => {
-    return str1 + str2
+  if (str1 === undefined || str2 === undefined) throw new Error('Please provide two strings')
+  return str1 + str2
 }
 
-// const height = parseInt(process.argv[2])
-// const hollow = (process.argv[3])
-// function print_tree(height, hollow) {
-//   if (height < 4 ) {
-//     return console.log('Tree must be larger than 4')
-//   }
-//   const width = determineWidth(height)
-//   let numXs = 1
-//
-//   for (let i = 0; i < height; i++) {
-//     if (i + 1 === height) {
-//       numXs = 1
-//       break
-//     }
-//     console.log(genLine(width, i + numXs))
-//     numXs++
-//   }
-//   console.log(genLine(width, numXs))
-// }
-// const width = ((height - 1) * 2) - 1
-//
-// for (let i = 0; i < height; i++) {
-//   const lastLine = i + 1 === height
-//   const numberOfXes = lastLine ? 1 : i + i + 1
-//   console.log(genLine(width, numberOfXes))
-// }
-// }
-//
-// function genLine(width, numXs){
-//   const leftOffset = Math.floor((width - numXs)/2)
-//   const rightOffset = numXs + leftOffset
-//
-//   if (width === numXs) return Array(width).fill('X').join('')
-//
-//   return Array(width).fill().map((_, cellIndex) =>
-//     cellIndex < leftOffset ||
-//     (hollow && cellIndex > leftOffset && cellIndex < rightOffset - 1 ) ||
-//     cellIndex >= rightOffset ? '-' : 'X'
-//   ).join('')
-// }
-//
-// print_tree(height, hollow)
+const squareNumber = (num) => {
+  if (num === undefined) throw new Error('Please provide a number')
+  return num * num
+}
 
 module.exports = {
-  concatStrings
+  concatStrings,
+  squareNumber
 }
--- test/unit_test.js
+++ test/unit_test.js
@@ -1,9 +1,11 @@
 const chai              = require('chai')
 const expect            = chai.expect
-const { concatStrings } = require('../public/javascripts/bitEngine.js')
+const { concatStrings,
+        squareNumber
+                      } = require('../public/javascripts/bitEngine.js')
 
-describe('Unit Tests for functions in public/javascripts/bitEngine.js', function() {
-  context('concatString', function() {
+describe('Unit Tests for bitEngine', function() {
+  context('concatStrings', function() {
     it('concats two strings', function() {
       expected = "This is a concated string"
       result = concatStrings('This is a ', 'concated string')
@@ -11,100 +13,24 @@ describe('Unit Tests for functions in public/javascripts/bitEngine.js', function
     })
 
     it('returns a string', function() {
-      expected = 'string'
       result = typeof concatStrings('I am part ', 'of a string')
+      expect(result).to.equal('string')
+    })
+
+    it('throws an error if no string is provided', function() {
+      expect(concatStrings).to.throw('Please provide two strings')
+    })
+  })
+
+  context('squareNumber', function() {
+    it('squares a number', function(){
+      expected = 16
+      result = squareNumber(4)
       expect(result).to.equal(expected)
+    })
+
+    it('throws an error if no number is provided', function() {
+      expect(squareNumber).to.throw('Please provide a number')
     })
   })
 })
-
-
-// const chai                = require('chai')
-// const expect              = chai.expect
-// const { resetDatabase }   = require('./helpers.js')
-// const { findHistory,
-//   findUser,
-//   createUser,
-//   saveSearch,
-//   findAll,
-//   findSearchTerm, } = require('../database/queries')
-//
-// describe('Database Queries', function() {
-//   beforeEach(resetDatabase)
-//   afterEach(resetDatabase)
-//
-//   context('findHistory', function() {
-//     it('Returns the history of a user', function() {
-//       return findHistory('Ben Bracamonte')
-//         .then(records => {
-//           expect(records[0].search_term).to.equal('The Matrix')
-//           expect(records.length).to.equal(3)
-//         })
-//         .then(() =>{
-//           return findHistory('Jenna Wieden')
-//             .then(records => {
-//               expect(records[1].search_term).to.equal('Jurassic Park')
-//               expect(records.length).to.equal(2)
-//             })
-//         })
-//         .catch(e => { throw e })
-//     })
-//   })
-//
-//   context('findUser', function() {
-//     it('Returns the user name', function() {
-//       return findUser('Jenna Wieden')
-//         .then(records => {
-//           expect(records.users_name).to.equal('Jenna Wieden')
-//         })
-//         .catch(e => { throw e })
-//     })
-//
-//     it('Returns the user name of a different user', function() {
-//       return findUser('Ben Bracamonte')
-//         .then(records => {
-//           expect(records.users_name).to.equal('Ben Bracamonte')
-//         })
-//         .catch(e => { throw e })
-//     })
-//   })
-//
-//   context('createUser', function() {
-//     it('Creates a user and adds it to the database', function() {
-//       return createUser({
-//         name: 'Lenny Dogface',
-//         password: 'password',
-//       })
-//         .then(() => {
-//           return findUser('Lenny Dogface')})
-//         .then(records => {
-//           expect(records.users_name).to.equal('Lenny Dogface')
-//         })
-//         .catch(e => { throw e })
-//     })
-//   })
-//
-//   context('saveSearch', function() {
-//     it('Saves a new search entry for an existing search term to the database', function() {
-//       return saveSearch({searchTerm: 'The Joy Luck Club', user: 'Jenna Wieden'})
-//         .then(() =>{
-//           return findHistory('Jenna Wieden')
-//             .then(records => {
-//               expect(records.length).to.equal(3)
-//             })
-//         })
-//         .catch( e => { throw e })
-//     })
-//
-//     it('Saves a new search term and search entry to the database', function() {
-//       return saveSearch({searchTerm: 'A new test movie!', user: 'Ben Bracamonte'})
-//         .then(() => {
-//           return findHistory('Ben Bracamonte')
-//             .then(records => {
-//               expect(records.length).to.equal(4)
-//             })
-//         })
-//         .catch( e => { throw e })
-//     })
-//   })
-// })
